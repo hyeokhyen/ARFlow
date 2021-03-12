f14ac3e8718e495cec447b82308e0be8ac84fe9c
hyeok kwon <hyeokhyen@gatech.edu>
2021-03-12 15:41:35 -0500

    inference demo

--- inference.py
+++ inference.py
@@ -6,18 +6,24 @@ import matplotlib.pyplot as plt
 import torch
 from easydict import EasyDict
 from torchvision import transforms
-from transforms import sep_transforms
-
-from utils.flow_utils import flow_to_image, resize_flow
-from utils.torch_utils import restore_model
-from models.pwclite import PWCLite
+try:
+    from transforms import sep_transforms
+    from utils.flow_utils import flow_to_image, resize_flow
+    from utils.torch_utils import restore_model
+    from models.pwclite import PWCLite
+except:
+    from .transforms import sep_transforms
+    from .utils.flow_utils import flow_to_image, resize_flow
+    from .utils.torch_utils import restore_model
+    from .models.pwclite import PWCLite
 
 
 class TestHelper():
-    def __init__(self, cfg):
+    def __init__(self, cfg, device):
         self.cfg = EasyDict(cfg)
-        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
-            "cpu")
+        self.cfg.model.device = device
+        self.device = device
+        # self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")        
         self.model = self.init_model()
         self.input_transform = transforms.Compose([
             sep_transforms.Zoom(*self.cfg.test_shape),
@@ -28,8 +34,8 @@ class TestHelper():
     def init_model(self):
         model = PWCLite(self.cfg.model)
         # print('Number fo parameters: {}'.format(model.num_parameters()))
+        model = restore_model(model, self.cfg.pretrained_model, self.device)
         model = model.to(self.device)
-        model = restore_model(model, self.cfg.pretrained_model)
         model.eval()
         return model
 
@@ -51,16 +57,23 @@ if __name__ == '__main__':
         'model': {
             'upsample': True,
             'n_frames': len(args.img_list),
-            'reduce_dense': True
+            'reduce_dense': True,
+            'device': None
         },
         'pretrained_model': args.model,
         'test_shape': args.test_shape,
     }
 
-    ts = TestHelper(cfg)
+    device = torch.device("cpu")
+
+    ts = TestHelper(cfg, device)
 
     imgs = [imageio.imread(img).astype(np.float32) for img in args.img_list]
     h, w = imgs[0].shape[:2]
+    print (h,w)
+    print (imgs[0].shape)
+    # print (imgs)
+    assert False
 
     flow_12 = ts.run(imgs)['flows_fw'][0]
 
@@ -70,5 +83,7 @@ if __name__ == '__main__':
     vis_flow = flow_to_image(np_flow_12)
 
     fig = plt.figure()
-    plt.imshow(vis_flow)
-    plt.show()
+    plt.imsave('examples/result.png', vis_flow)
+    print ('save in ... examples/result.png')
+    # plt.imshow(vis_flow)
+    # plt.show()
